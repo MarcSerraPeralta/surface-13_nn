5de2592a5ef61cc8b5ffdf4944449632507c4960
MarcSerraPeralta <marcserraperalta@gmail.com>
2024-02-22 17:41:37 +0100

    Add comments and rm command

--- run_commands.sh
+++ run_commands.sh
@@ -1,4 +1,10 @@
+# install requirements
+# NB: it is recommended to create a virtual environment
 pip install -r requirements.txt
 
+# run processing and splitting for NN datasets (train, dev, test)
 python processing_data_for_NN_IQ.py
 python split_train_val_test.py
+
+# remove duplicated data
+rm -rf nn_data/all/
