bcbff2414757728e00c34860f757b3c0785227a1
MarcSerraPeralta <marcserraperalta@gmail.com>
2024-02-23 17:59:43 +0100

    Simplify processing of data

--- README.md
+++ README.md
@@ -4,6 +4,9 @@ Processing of the experimental data from a Surface-13 experiment for training NN
 # 1) Process experimental data
 
 The experimental datasets (`npy` files) should be copied/moved to the `delft_data` directory. 
-Run the commands in `run_commands.sh` (this can be done in Unix systems by executing `./run_commands.sh` in the bash terminal). 
+Create a virtual environment and install the requirements using `pip install -r requirements.txt`. 
+Process the data by running `python process_data.py` and remove the duplicated data in `nn_data/all` (in Unix systems this can be done by `rm -rf nn_data/all/`). 
 The training, validation, and testing datasets will be stored in the new `nn_data` directory. 
+
+# 2) Evaluate the NNs
 
--- process_data.py
+++ process_data.py
@@ -3,10 +3,10 @@
 print("Importing libraries...")
 
 import numpy as np
-import matplotlib.pyplot as plt
 import xarray as xr
 import pathlib
 import os
+import random
 
 from delft_data.process_data import process_data
 
@@ -21,8 +21,18 @@ MEAS_TYPE = float
 ROUNDS = ["1_R", "2_R", "4_R", "8_R", "16_R"]
 
 OUTPUT_PREFIX = "rot_surf-code-13_DiCarlo_qubit_IQ_leak_bZ_s{init}_r{rounds}"
-OUTPUT_DIR = pathlib.Path("nn_data/all")
-OUTPUT_DIR.mkdir(exist_ok=True, parents=True)
+OUTPUT_DIR = pathlib.Path("nn_data")
+
+PARTITIONS = {
+    "test": 5_000,  # number of shots reserved for testing
+    "train": 0.90,  # percentage of the remaining shots
+    "dev": None,  # all the remaining shots
+}
+
+
+###################################
+
+(OUTPUT_DIR / "all").mkdir(exist_ok=True, parents=True)
 
 
 # Process data
@@ -198,10 +208,84 @@ for filename in FILE_NAMES:
 
         # STORE DATASET
         init_bitstring = "".join(map(str, data_init.astype(int)))
-        FOLDER = OUTPUT_DIR / OUTPUT_PREFIX.format(
-            init=init_bitstring, rounds=rounds_int
+        FOLDER = (
+            OUTPUT_DIR
+            / "all"
+            / OUTPUT_PREFIX.format(init=init_bitstring, rounds=rounds_int)
         )
         FOLDER.mkdir(exist_ok=True)
         dataset.to_netcdf(FOLDER / "measurements.nc")
         da_leakage_ps.to_netcdf(FOLDER / "leakage_ps.nc")
     print("\n")
+
+# Split data into training, validation, and testing
+
+FOLDERS = sorted(os.listdir(OUTPUT_DIR / "all"))
+
+np.random.seed(9999)  # reproducible results
+
+for partition in PARTITIONS:
+    DATASET_PATH = OUTPUT_DIR / partition
+    DATASET_PATH.mkdir(exist_ok=True)
+
+TOTAL_SHOTS = {k: 0 for k in PARTITIONS}
+
+for folder in FOLDERS:
+    print(folder, end="\r")
+    dataset = xr.load_dataset(OUTPUT_DIR / "all" / folder / "measurements.nc")
+
+    total_shots = len(dataset.shot)
+    used_shots = np.zeros(total_shots).astype(bool)  # mask = 1 the shot has been used
+
+    # TEST
+    non_used_idx = np.nonzero(~used_shots)[0]
+    assert len(non_used_idx) >= PARTITIONS["test"]
+    np.random.shuffle(non_used_idx)
+    test_idx = non_used_idx[: PARTITIONS["test"]]
+    partition_dataset = dataset.isel(shot=test_idx)
+
+    # save
+    DATASET_PATH = OUTPUT_DIR / "test" / folder
+    DATASET_PATH.mkdir(exist_ok=True)
+    partition_dataset.to_netcdf(DATASET_PATH / "measurements.nc")
+    TOTAL_SHOTS["test"] += len(test_idx)
+
+    # update used shots
+    used_shots[test_idx] = True
+
+    # TRAIN
+    non_used_idx = np.nonzero(~used_shots)[0]
+    np.random.shuffle(non_used_idx)
+    train_idx = non_used_idx[: int(len(non_used_idx) * PARTITIONS["train"])]
+    partition_dataset = dataset.isel(shot=train_idx)
+
+    # save
+    DATASET_PATH = OUTPUT_DIR / "train" / folder
+    DATASET_PATH.mkdir(exist_ok=True)
+    partition_dataset.to_netcdf(DATASET_PATH / "measurements.nc")
+    TOTAL_SHOTS["train"] += len(train_idx)
+
+    # update used shots
+    used_shots[train_idx] = True
+
+    # DEV
+    non_used_idx = np.nonzero(~used_shots)[0]
+    dev_idx = non_used_idx
+    partition_dataset = dataset.isel(shot=dev_idx)
+
+    # save
+    DATASET_PATH = OUTPUT_DIR / "dev" / folder
+    DATASET_PATH.mkdir(exist_ok=True)
+    partition_dataset.to_netcdf(DATASET_PATH / "measurements.nc")
+    TOTAL_SHOTS["dev"] += len(dev_idx)
+
+    # update used shots
+    used_shots[dev_idx] = True
+
+    # ASSERTIONS
+    assert used_shots.all()
+
+
+print("\nTOTAL NUMBER OF SHOTS")
+for key, value in TOTAL_SHOTS.items():
+    print(key, value)
